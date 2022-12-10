b12ca3abd0e44cda898b02536813828c6ea374ab
seoyoonOh <5happy1004kr@naver.com>
2022-12-10 22:29:20 +0900

    ✨ 할 일 삭제 및 추가 기능

--- src/App.js
+++ src/App.js
@@ -19,18 +19,42 @@ export default class App extends Component {
     };
   };
 
-  todoData = [
-    {
-      id: "1",
-      title: "공부하기",
-      completed: true,
-    },
-    {
-      id: "2",
-      title: "청소하기",
+  state = {
+    todoData: [
+      {
+        id: "1",
+        title: "공부하기",
+        completed: true,
+      },
+      {
+        id: "2",
+        title: "청소하기",
+        completed: false,
+      },
+    ],
+    value: "",
+  };
+
+  handleClick = (id) => {
+    let newTodoData = this.state.todoData.filter((data) => data.id !== id);
+    this.setState({ todoData: newTodoData });
+  };
+
+  handleChange = (e) => {
+    this.setState({ value: e.target.value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    let newTodo = {
+      id: Date.now(),
+      title: this.state.value,
       completed: false,
-    },
-  ];
+    };
+
+    this.setState({ todoData: [...this.state.todoData, newTodo] });
+  };
 
   render() {
     return (
@@ -40,12 +64,19 @@ export default class App extends Component {
             <h1>할 일 목록</h1>
           </div>
 
-          {this.todoData.map((data) => (
+          {this.state.todoData.map((data) => (
             <div style={this.getStyle()} key={data.id}>
               <input type="checkbox" defaultChecked={data.completed} /> {data.title}
-              <button style={this.btnStyle}>X</button>
+              <button style={this.btnStyle} onClick={() => this.handleClick(data.id)}>
+                X
+              </button>
             </div>
           ))}
+
+          <form style={{ display: "flex" }} onSubmit={this.handleSubmit}>
+            <input type="text" name="value" style={{ flex: "10", padding: "5px" }} placeholder="해야 할 일을 입력하세요." value={this.state.value} onChange={this.handleChange} />
+            <input type="submit" value="입력" className="btn" style={{ flex: "1" }} />
+          </form>
         </div>
       </div>
     );
